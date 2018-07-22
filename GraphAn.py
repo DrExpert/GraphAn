f764ebff7fb72591ca1e264a905baad9948460f3
DrExpert <czlowiekkanapa@gmail.com>
2018-07-22 11:48:23 +0200

    carving

--- GraphAn.py
+++ GraphAn.py
@@ -4,19 +4,23 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import math as math
 
-resolution = 3000
-size = 2400
 
+_probe = 1072
+_size = 16
+_fps = 1
+_time =1
+_t0 = 1
+_tn = 3
 
 #here specify your function
 def func(lenght, t):
     
     (x_min, x_max) = (-lenght, lenght)
     (y_min, y_max) = (-lenght, lenght)
-    X, Y = np.meshgrid(np.linspace(x_min, x_max, lenght/2),
-                       np.linspace(y_min, y_max, lenght/2))
+    x, y = np.meshgrid(np.linspace(x_min, x_max, _probe),
+                       np.linspace(y_min, y_max, _probe))
 	
-    Z = 5*np.sin((x)*np.sin(y)*np.sin(y*x+(t/10)))
+    Z = 20*np.power(np.power((np.mod(x,8)-4)*10,2)+np.power((np.mod(y,8)-4)*10,2),1/2)
     return Z.astype(dtype=np.int32)
 
 
@@ -24,7 +28,7 @@ def func(lenght, t):
 
 def update(args):
     
-    im.set_data(func(size, args))
+    im.set_data(func(_size, args))
     return im,
 
 
@@ -32,12 +36,12 @@ if __name__ == '__main__':
     writer = animation.writers['ffmpeg']
 	
 	#you may want to change the artist part here
-    writer = writer(fps=15, metadata=dict(artist='Anonymouse'), bitrate=80000)
-    a = func(size, 0)
+    writer = writer(fps=_fps, metadata=dict(artist='Anonymouse'))
+    a = func(_size, 0)
     fig = plt.figure()
     plt.axis('off')
     
-    im = plt.imshow(a, animated=True)
-    ani = animation.FuncAnimation(fig, update, np.arange(1, 200),
+    im = plt.imshow(a, animated=True,cmap=plt.get_cmap('inferno'))
+    ani = animation.FuncAnimation(fig, update, np.linspace(_t0, _tn, _time*_fps),
                                   interval=1)
-    ani.save('generated.mp4', writer=writer)
+    ani.save('2.mp4', writer=writer)
